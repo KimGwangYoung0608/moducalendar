c79642d7dcb62b635c843af2db7418f327605b6d
KimGwangYoung0608 <genspark_flutter_dev@genspark.ai>
2026-01-26 15:17:45 +0000

    🔧 Update repo name to moducalendar

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/MODU-calendar/",
+  base: "/moducalendar/",
   server: {
     host: "::",
     port: 8080,
